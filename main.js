883cea316652e9a4cef68826cf2136912d9521dd
KoenLav <k.lavrijssen@gmail.com>
2018-07-21 15:41:38 +0200

    master | Updated v8-profiler

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-import './profile.js';
 import fs from 'fs';
+import './profile.js';
 
 let v8Profiler = Npm.require('v8-profiler-node8'),
-    { startProfile, stopProfile } = getProfiler(v8Profiler, fs),
-    profileDuration = (profileName, exportPath, duration) => {
-      startProfile(profileName, { exportPath });
-      setTimeout(() => stopProfile(profileName), duration);
-    }
-;
+  { startProfile, stopProfile } = getProfiler(v8Profiler, fs),
+  profileDuration = (profileName, exportPath, duration) => {
+    startProfile(profileName, { exportPath });
+    setTimeout(() => stopProfile(profileName), duration);
+  };
 
 export default {
   startProfile,
